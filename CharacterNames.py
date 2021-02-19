acfcdaec3c7ac8e283605bd60be58370b8caad64
Amin <79174535+ImZadeQasemzade@users.noreply.github.com>
2021-02-19 11:59:51 -0500

    Add files via upload

--- CharacterNames.py
+++ CharacterNames.py
@@ -2,25 +2,29 @@ import os
 import nltk
 from nameparser.parser import HumanName
 import networkx as nx
+import spacy  # python -m spacy download en
+import en_core_web_sm
 
 
+# TODO Improve names extraction: NLTK, Stanford, SpaCy, Google
+# TODO Words in the first of the sentence
 # TODO Improve the similarity condition
-# TODO Improve names extraction: Try stanford for names extraction
-# TODO Names with size 1 or less are IGNORED in get_character_names
+# TODO Cleaning SpaCy{}
+# TODO Describing the link
 
 
-def get_character_names(text):
+def get_character_names(text):  # using nltk
     """
     :param text: raw text
     :return: list of strings of all names
     """
     tokens = nltk.tokenize.word_tokenize(text)
     pos = nltk.pos_tag(tokens)
-    sentt = nltk.ne_chunk(pos, binary=False)
+    sent = nltk.ne_chunk(pos, binary=False)
     person = []
     person_list = []
     name = ""
-    for subtree in sentt.subtrees(filter=lambda t: t.label() == 'PERSON'):
+    for subtree in sent.subtrees(filter=lambda t: t.label() == 'PERSON'):
         for leaf in subtree.leaves():
             person.append(leaf[0])
         if len(person) > 1:  # avoid grabbing lone surnames
@@ -33,18 +37,18 @@ def get_character_names(text):
     return person_list
 
 
-def get_character_names_v2(text):
+def get_character_names_v2(text):  # using nltk
     """
     :param text: raw text
     :return: list of strings of all names
     """
     tokens = nltk.tokenize.word_tokenize(text)
     pos = nltk.pos_tag(tokens)
-    sentt = nltk.ne_chunk(pos, binary=False)
+    sent = nltk.ne_chunk(pos, binary=False)
     person = []
     person_list = []
     name = ""
-    for subtree in sentt.subtrees(filter=lambda t: t.label() == 'PERSON'):
+    for subtree in sent.subtrees(filter=lambda t: t.label() == 'PERSON'):
         for leaf in subtree.leaves():
             person.append(leaf[0])
         for part in person:
@@ -56,22 +60,61 @@ def get_character_names_v2(text):
     return person_list
 
 
+def get_character_names_v3(text):  # using spacy
+    # Create Doc object
+    nlp = en_core_web_sm.load()
+    doc = nlp(text)
+
+    # Identify the persons
+    persons = [ent.text for ent in doc.ents if ent.label_ == 'PERSON']
+
+    persons = similarity_filtering_names(persons)
+
+    # Return persons
+    return persons
+
+
+def similarity_filtering_names(names):
+    filtered_people = []
+    nlp = en_core_web_sm.load()
+
+    """# removing verbs
+    for name in names:
+        doc = nlp(name)
+        for ent in doc.ents:
+            if ent.label_ == 'VERB':
+                names.remove(name)
+    """
+
+    # removing 's
+    for name in names:
+        if name[-2:] == "'s":
+            names[names.index(name)] = name.replace("'s", "")
+
+    """for name in names:
+        first_last = HumanName(name).first + ' ' + HumanName(name).last
+        print(first_last)
+        filtered_people.append(name)
+    return filtered_people"""
+    return names
+
+
 def print_character_names(book_address):
     """
     prints all characters' name and last name found in the book
     """
     book_read = open(book_address, "r")
     # whole book as a string
-    lis = book_read.readlines()
-    stri = ""
-    for i in range(len(lis)):
-        stri = stri + lis[i]
+    book_lines = book_read.readlines()
+    text = ""
+    for i in range(len(book_lines)):
+        text = text + book_lines[i]
 
-    names = get_character_names_v2(stri)
-    print("LAST, FIRST")
+    names = get_character_names_v2(text)
+    print("FIRST LAST")
     for name in names:
-        last_first = HumanName(name).last + ', ' + HumanName(name).first
-        print(last_first)
+        first_last = HumanName(name).first + ' ' + HumanName(name).last
+        print(first_last)
 
 
 def create_character_graph(book_address):
@@ -83,16 +126,16 @@ def create_character_graph(book_address):
     """
     book_read = open(book_address, "r")
     # whole book as a string
-    stri = ""
+    text = ""
     names = []
     counts = []
     graph = nx.Graph()
     for li, line in enumerate(book_read.readlines()):
-        stri = stri + line
+        text = text + line
         if li % 30 == 1:
-            new_names = get_character_names_v2(stri)
+            new_names = get_character_names_v3(text)
             for name in new_names:
-                if name not in names:  # TODO for loop and check the name exists or not
+                if name not in names:
                     names.append(name)
                     counts.append(1)
                     graph.add_node(names.index(name))
@@ -103,9 +146,12 @@ def create_character_graph(book_address):
                     ind_i = names.index(new_names[i])
                     ind_j = names.index(new_names[j])
                     graph.add_edge(ind_i, ind_j)
-            stri = ""
+            text = ""
+    print(graph.edges())
+    print(graph.nodes())
     return graph, names, counts
 
 
 if __name__ == '__main__':
-    create_character_graph(book_address="./Data/Gutenberg/txt/Charles Dickens___A Christmas Carol.txt")
+    create_character_graph(book_address="C:/Users/amin/OneDrive - York University/Courses/Data analysis and "
+                                        "visualization/Projects/Gutenberg/txt/Charles Dickens___A Christmas Carol.txt")
--- script.py
+++ script.py
@@ -1,10 +1,11 @@
 from CharacterNames import create_character_graph
 from DrawGraph import draw
 
-adr = "./Data/Gutenberg/txt/Charles Dickens___A Christmas Carol.txt"
+book_address = "C:/Users/amin/OneDrive - York University/Courses/Data analysis and " \
+               "visualization/Projects/Gutenberg/txt/Charles Dickens___A Christmas Carol.txt "
 
 print("creating graph")
-G, names, counts = create_character_graph(adr)
+G, names, counts = create_character_graph(book_address)
 print("name\t count")
 for i in range(len(names)):
     print(f'{names[i]}\t{counts[i]}')
